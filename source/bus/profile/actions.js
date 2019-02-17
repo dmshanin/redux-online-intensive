f3f180be115e5674f3a9436879e9e5e828d176d8
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2019-02-18 01:57:58 +0300

    Redux Online 🚀 Урок 5-6

--- source/bus/profile/actions.js
+++ source/bus/profile/actions.js
@@ -4,7 +4,7 @@ import { types } from "./types";
 export const profileActions = {
     fillProfile: (profile) => {
         return {
-            type:    types.FILL_POSTS,
+            type:    types.FILL_PROFILE,
             payload: profile,
         };
     },
